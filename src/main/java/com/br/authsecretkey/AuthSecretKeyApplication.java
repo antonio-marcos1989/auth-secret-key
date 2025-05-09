98b474c16a7719c3bc237a845f04846088c4fd9e
Antonio Marcos Alves Morais <115155873+antonio-marcos1989@users.noreply.github.com>
2025-05-09 19:16:23 -0300

    Add

---
+++ src/main/java/com/br/authsecretkey/AuthSecretKeyApplication.java
@@ -0,0 +1,13 @@
+package com.br.authsecretkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSecretKeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthSecretKeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/authsecretkey/AuthSecretKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.authsecretkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSecretKeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
